bbc057027a3165eeaea1eac0d2c6f888f6d1ddd2
dawuah3 <derrickawuah@hotmail.com>
2018-02-26 21:40:35 -0500

    Get Features controller

---
+++ Controllers/FeaturesController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Persistence;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using vega.Models;
+
+namespace vega.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public FeaturesController(VegaDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await _context.Features.ToListAsync();
+
+            return _mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+    }
+}
