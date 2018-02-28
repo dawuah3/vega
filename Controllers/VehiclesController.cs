73ce394fec0bc47eff58d5f4bcd380b7c8b5a823
dawuah3 <derrickawuah@hotmail.com>
2018-02-27 21:50:39 -0500

    Added validation to create vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -22,9 +22,19 @@ namespace vega.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource vehicleResource)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            if (model == null)
+            {
+                ModelState.AddModelError("ModelId", "Invalid modelId.");
+                return BadRequest(ModelState);
+            }
+            
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdated = DateTime.Now;
-            
+
             context.Vehicles.Add(vehicle);
             await context.SaveChangesAsync();
             
